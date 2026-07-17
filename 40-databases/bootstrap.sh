bb3c7338b7e88cce784d5cdc3f12d051f36393d9
VinodDevOpsTech <vinodkumar160107@gmail.com>
2026-07-18 00:34:31 +0530

    hello

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,14 +1,15 @@
-component = $1
-environment = $2
-sudo dnf install ansible -y
-mkdir /var/log/roboshop
+#!/bin/bash
+
+component=$1 #mongodb
+environment=$2 #dev
+dnf install ansible -y
+mkdir -p /var/log/roboshop/
 chown -R ec2-user:ec2-user /var/log/roboshop
 chmod -R 755 /var/log/roboshop
 touch /var/log/roboshop/ansible.log
 
 cd /home/ec2-user
-git clone https://github.com/VinodDevOpsTech/ansible-roboshop-v3.git
-cd ansible-roboshop-v3
+git clone https://github.com/VinodDevOpsTech/roboshop-ansible-v3.git
+cd roboshop-ansible-v3
 git pull
-
 ansible-playbook -e component=$component -e env=$environment roboshop.yaml
