4fda4a359df760e52c66638c0cee5760b28dc35f
VinodDevOpsTech <vinodkumar160107@gmail.com>
2026-07-18 12:44:21 +0530

    hello

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -1,5 +1,8 @@
 locals {
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+    redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
+    mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
+    rabbitmq_sg_id = data.aws_ssm_parameter.rabbit_sg_id.value
     common_name = "${var.project}-${var.environment}"
     database_subnet_ids = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
     common_tags ={
