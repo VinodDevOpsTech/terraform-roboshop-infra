7ae7fe814f9c05eb2fd167b53f29047d64b04f76
VinodDevOpsTech <vinodkumar160107@gmail.com>
2026-07-19 12:34:56 +0530

    hello

---
+++ 50-backend-alb/locals.tf
@@ -0,0 +1,9 @@
+locals {
+    backend_alb_sg_id= data.aws_ssm_parameter.backend_alb_sg_id.id
+    common_name = "${var.project}-${var.environment}"
+    private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    common_tags ={
+        Project = "${var.project}"
+        Environment = "${var.environment}"
+    }
+}
---
+++ 50-backend-alb/main.tf
@@ -0,0 +1,45 @@
+resource "aws_lb" "backend_alb" {
+    name               = "${local.common_name}-backend-alb"
+    internal           = false
+    load_balancer_type = "application"
+    security_groups    = [local.backend_alb_sg_id]
+    subnets            = local.private_subnet_ids
+
+    enable_deletion_protection = true
+
+    tags = merge(
+        local.common_tags,
+        {
+            Name = "${local.common_name}-backend-alb"
+        }
+    )
+}
+
+resource "aws_lb_listener" "http" {
+    load_balancer_arn = aws_lb.backend_alb.arn
+    port              = "80"
+    protocol          = "HTTP"
+
+    default_action {
+        type = "fixed-response"
+
+        fixed_response {
+        content_type = "text/html"
+        message_body = "<h1>Hi, I am vinod, i am learning DevOps</h1>"
+        status_code  = "200"
+        }
+    }
+}
+
+resource "aws_route53_record" "www" {
+    zone_id = var.zone_id
+    name    = "*.backend_alb-${var.environment}.${var.domain_name}"
+    type    = "A"
+
+    alias {
+        name                   = aws_elb.backend_alb.dns_name
+        zone_id                = aws_elb.backend_alb.zone_id
+        evaluate_target_health = true
+    }
+    allow_overwrite = true
+}
