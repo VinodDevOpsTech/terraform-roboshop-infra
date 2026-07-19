2c4a3da1f05a6f97dfc5fae56ac9817db8986f12
VinodDevOpsTech <vinodkumar160107@gmail.com>
2026-07-19 23:34:58 +0530

    hello

--- 60-catalogue/provaider.tf
+++ 60-catalogue/provaider.tf
@@ -10,7 +10,7 @@ terraform {
     # remote state storage location
     backend "s3" {
         bucket         = "remote-state-maxdevopstech"
-        key            = "roboshop-backend-ald.tfstate"
+        key            = "roboshop-catalogue.tfstate"
         region         = "us-east-1"
         encrypt        = true
         use_lockfile   = true # Enables native S3 state locking (Terraform 1.10+)
--- 60-catalogue/variabels.tf
+++ 60-catalogue/variabels.tf
@@ -5,7 +5,7 @@ variable "environment" {
     default = "dev"
 }
 variable "app_version" {
-    default = v3
+    default = "v3"
 }
 /* variable "zone_id" {
     default = "Z0584760TQ3FQFBERSRB"
