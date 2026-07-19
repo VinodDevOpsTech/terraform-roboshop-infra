6b5dbab477fba2bd51518d4ae851d5dc73273a61
VinodDevOpsTech <vinodkumar160107@gmail.com>
2026-07-19 12:50:16 +0530

    hello

--- 50-backend-alb/locals.tf
+++ 50-backend-alb/locals.tf
@@ -1,5 +1,5 @@
 locals {
-    backend_alb_sg_id= data.aws_ssm_parameter.backend_alb_sg_id.id
+    backend_alb_sg_id= data.aws_ssm_parameter.backend_alb_sg_id.value
     common_name = "${var.project}-${var.environment}"
     private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
     common_tags ={
