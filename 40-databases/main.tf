5765a42183e484d65a433d9abc4fa415255b088e
VinodDevOpsTech <vinodkumar160107@gmail.com>
2026-07-19 11:45:30 +0530

    hello

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -85,7 +85,7 @@ resource "terraform_data" "redis" {
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.mysql_sg_id]
     subnet_id = local.database_subnet_ids
-    
+    iam_instance_profile = aws_iam_instance_profile.mysql.name
     
     tags = merge(
         {
