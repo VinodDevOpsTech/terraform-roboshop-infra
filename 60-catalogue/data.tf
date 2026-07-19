86c3b20613c6f6ac35b73438430bf4cc3361b9d8
VinodDevOpsTech <vinodkumar160107@gmail.com>
2026-07-19 23:37:52 +0530

    hello

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -4,7 +4,7 @@ data "aws_ssm_parameter" "private_subnet_ids" {
     name = "/${var.project}/${var.environment}/private_subnet_ids"
 }
 
-data "aws_ssm_parameter" "catalogue_alb_sg_id" {
+data "aws_ssm_parameter" "catalogue_sg_id" {
     name = "/${var.project}/${var.environment}/catalogue_alb_sg_id"
 }
 data "aws_ami" "DevOps" {
