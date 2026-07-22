a615bda365499279c4ee89f15118bfce32785a30
VinodDevOpsTech <vinodkumar160107@gmail.com>
2026-07-22 23:30:24 +0530

    hello

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -2,7 +2,7 @@ locals {
     catalogue_sg_id= data.aws_ssm_parameter.catalogue_sg_id.value
     ami = data.aws_ami.DevOps.id
     common_name = "${var.project}-${var.environment}"
-    private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     common_tags ={
         Project = "${var.project}"
