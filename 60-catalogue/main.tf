b393cd3cba5a2d51749896ee00d29da2b74b9b5e
VinodDevOpsTech <vinodkumar160107@gmail.com>
2026-07-22 21:28:47 +0530

    hello

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -58,3 +58,36 @@ resource "aws_ami_from_instance" "catalogue" {
     )
 }
 
+
+
+resource "aws_launch_template" "catalogue" {
+    name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
+    image_id = aws_ami_from_instance.catalogue.id
+    instance_initiated_shutdown_behavior = "terminate"
+    instance_type = "t3.micro"
+    update_default_version = true
+    vpc_security_group_ids = [local.catalogue_sg_id]
+
+
+    tag_specifications {
+        resource_type = "instance"
+
+        tags = merge(
+            local.common_tags,
+        {
+        Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
+        }
+        )
+    }
+    tag_specifications {
+        resource_type = "volume"
+
+        tags = merge(
+            local.common_tags,
+        {
+        Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
+        }
+        )
+    }
+
+    }
