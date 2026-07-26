41fa2f3708ed41c558aee102d1076b117059e368
VinodDevOpsTech <vinodkumar160107@gmail.com>
2026-07-26 13:38:04 +0530

    hello

--- 50-backend-alb/parameter.tf
+++ 50-backend-alb/parameter.tf
@@ -1,4 +1,4 @@
-resource "aws_ssm_parameter" "alb_listener" {
+resource "aws_ssm_parameter" "backend_alb_listener_arn" {
     name  = "/${var.project}/${var.environment}/backend_alb_listener_arn"
     type  = "String"
     value = aws_lb_listener.http.arn
